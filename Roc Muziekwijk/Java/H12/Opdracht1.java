8d35eb8e8891015e7334bb446baf4183079221ef
thomkok18 <tkok@roc-dev.com>
2015-10-09 14:26:20 +0200

    Nieuwe opdracht. Ik ga ermee beginnen

---
+++ Roc Muziekwijk/Java/H12/Opdracht1.java
@@ -0,0 +1,28 @@
+// Hoofdstuk 12
+package H12;
+
+// Importeer de benodigde klassen uit de Java bibliotheek.
+import java.awt.*;
+import java.applet.*;
+
+// Een klasse met de naam Opdracht1 van het type Applet.
+public class Opdracht1 extends Applet {
+
+	// Een (lege) methode die de Applet gaat initialiseren.
+	public void init() {
+		setSize(300, 400);
+	}
+
+	// Een methode die de inhoud van het scherm tekent.	
+	public void paint(Graphics g) {
+		int teller;
+		int lengte = 0;
+		int breedte = 0;
+		for(teller = 1; teller <= 10; teller++) {
+			lengte += 20;
+			breedte += 20;
+			g.drawLine(breedte , 300, lengte, 50 );
+			g.drawString("" + teller, lengte , 315);
+		} 
+	}
+}
