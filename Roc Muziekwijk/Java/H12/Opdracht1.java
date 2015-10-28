5ffb1e0cbee816e4b9fe083ff0c728d3744afc3d
thomkok18 <tkok@roc-dev.com>
2015-10-28 13:24:49 +0100

    gemiddelde aangemaakt

--- Roc Muziekwijk/Java/H12/Opdracht1.java
+++ Roc Muziekwijk/Java/H12/Opdracht1.java
@@ -7,12 +7,15 @@ import java.applet.*;
 
 // Een klasse met de naam Opdracht1 van het type Applet.
 public class Opdracht1 extends Applet {
-	double salaris[];
+	double gemiddelde, getal;
+	int teller, salaris[];
+	String sGemiddelde;
 
 	// Een (lege) methode die de Applet gaat initialiseren.
 	public void init() {
 		setSize(400, 400);
-		salaris = new double[10];
+		sGemiddelde = "";
+		salaris = new int[10];
         for (int teller = 0; teller < salaris.length; teller ++) {
             salaris[teller] = 100 * teller + 100;
 	}
@@ -20,6 +23,8 @@ public class Opdracht1 extends Applet {
 
 	// Een methode die de inhoud van het scherm tekent.	
 	public void paint(Graphics g) {
+		g.drawString(sGemiddelde, 50, 80 );
+		gemiddelde = getal / teller;
 		 for (int teller = 0; teller < salaris.length; teller ++) {
 	            g.drawString("" + salaris[teller], 50, 20 * teller + 20);
 		 } 
