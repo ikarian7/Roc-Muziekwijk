126ca6e8564d817ffa05bc2a7409cd203d234646
thomkok18 <tkok@roc-dev.com>
2015-11-03 10:00:26 +0100

    Op naar opdracht 2

--- Roc Muziekwijk/Java/H12/Opdracht1.java
+++ Roc Muziekwijk/Java/H12/Opdracht1.java
@@ -7,26 +7,28 @@ import java.applet.*;
 
 // Een klasse met de naam Opdracht1 van het type Applet.
 public class Opdracht1 extends Applet {
-	double gemiddelde, getal;
-	int teller, salaris[];
-	String sGemiddelde;
+	double gemiddelde;
+	int teller, salaris[], optellen, beginGetal, totaalSalaris;
 
 	// Een (lege) methode die de Applet gaat initialiseren.
 	public void init() {
 		setSize(400, 400);
-		sGemiddelde = "";
+		optellen = 100;
+		beginGetal = 100;
+		totaalSalaris = 0;
 		salaris = new int[10];
         for (int teller = 0; teller < salaris.length; teller ++) {
-            salaris[teller] = 100 * teller + 100;
-	}
+            salaris[teller] = optellen * teller + beginGetal;
+            totaalSalaris += salaris[teller];
+        }
 }
 
 	// Een methode die de inhoud van het scherm tekent.	
 	public void paint(Graphics g) {
-		g.drawString(sGemiddelde, 50, 80 );
-		gemiddelde = getal / teller;
+		gemiddelde = totaalSalaris / salaris.length;
 		 for (int teller = 0; teller < salaris.length; teller ++) {
 	            g.drawString("" + salaris[teller], 50, 20 * teller + 20);
+	            g.drawString ("Gemiddelde is: " + gemiddelde, 50, 220); 
 		 } 
 	}
 }
---
+++ Roc Muziekwijk/Java/H12/Opdracht2.java
@@ -0,0 +1,16 @@
+// Hoofdstuk 12
+package H12;
+
+// Importeer de benodigde klassen uit de Java bibliotheek.
+import java.awt.*;
+import java.applet.*;
+
+// Een klasse met de naam Opdracht1 van het type Applet.
+public class Opdracht2 extends Applet {
+Button knop;
+
+	// Een (lege) methode die de Applet gaat initialiseren.
+	public void init() {
+		setSize(400, 400);
+    }
+}
