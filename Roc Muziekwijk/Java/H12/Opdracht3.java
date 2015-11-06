52a1214f1fd8e8938fef5eab1541a81d020aa53f
thomkok18 <tkok@roc-dev.com>
2015-11-06 10:38:26 +0100

    even een pauze

--- Roc Muziekwijk/Java/H12/Opdracht3.java
+++ Roc Muziekwijk/Java/H12/Opdracht3.java
@@ -8,7 +8,7 @@ import java.awt.event.*;
 
 // Een klasse met de naam Opdracht1 van het type Applet.
 public class Opdracht3 extends Applet {
-	int tekstvakken[];
+	int tekstvakken[], getal;
 	TextField tekstvak;
 	Button knopOk;
 
@@ -20,21 +20,30 @@ public class Opdracht3 extends Applet {
 		knopOk.addActionListener( new knopOkListener() );
 		add(knopOk);
 		for (int teller = 0; teller < tekstvakken.length; teller ++) {
-			tekstvakken[teller] = teller;
 			tekstvak = new TextField("");
 			tekstvak.addActionListener( new tekstvakListener() );
 			add(tekstvak);
 		}
 	}
-	
+
+	// Een methode die de inhoud van het scherm tekent.	
+	public void paint(Graphics g) {
+		for (int teller = 0; teller < tekstvakken.length; teller ++) {
+			g.drawString("" + tekstvakken[teller], 50, 20 * teller + 20);
+		}
+	}
+
 	class tekstvakListener implements ActionListener	{
 		public void actionPerformed( ActionEvent e ) {
+			getal = Integer.parseInt(tekstvak.getText());
+			tekstvak.setText("");
 			repaint();
 		}
 	}
-	
+
 	class knopOkListener implements ActionListener	{
 		public void actionPerformed( ActionEvent e ) {
+			getal = Integer.parseInt(tekstvak.getText());	
 			repaint();
 		}
 	}
